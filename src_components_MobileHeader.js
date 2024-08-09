d405c2d667cf480617b9048e1f425b2dae3edbed
robert1987b <robert1987brito@outlook.com>
2024-08-09 11:54:36 +0530

    Create src_components_MobileHeader.js

---
+++ src_components_MobileHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MobileHeader.css';
+
+function MobileHeader() {
+  return (
+    <header className="header">
+      <h1>CompassMobileDollarTree</h1>
+    </header>
+  );
+}
+
+export default MobileHeader;

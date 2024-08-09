ccd977ecb7ef0d5778b1959981c7c2560784a180
robert1987b <robert1987brito@outlook.com>
2024-08-09 11:58:40 +0530

    Create mobile_index.js

---
+++ src_mobile/mobile_index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './mobile_index.css';
+import MobileApp from './MobileApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MobileApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
